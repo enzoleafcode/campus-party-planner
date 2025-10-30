6497c8ec24ca4e6c369197ecb540c108dd991d67
Yosh64 <65314603+Yosh64@users.noreply.github.com>
2025-10-30 11:59:58 +0100

    Revert "feat: add CitySelector and EventCard components"

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,4 @@
-// function HomePage() {
-//   return <h1> Hello !</h1>;
-// }
-// export default HomePage;
-
-import CitySelector from "../components/CitySelector";
-import EventCard from "../components/EventCard";
-
-const Header = () => (
-  <header className="app-header">
-    <h1>Campus Party Planner</h1>
-    <CitySelector />
-    <EventCard/>
-  </header>
-);
-
-export default Header;
+function HomePage() {
+  return <h1> Hello !</h1>;
+}
+export default HomePage;
